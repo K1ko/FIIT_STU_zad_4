c5de84e354aa29386252621402bd68a73630bd49
Kristián Červenka <kristian.cervenka@gmail.com>
2022-10-13 13:16:19 +0200

    osme cvicenie

--- 120772_2022_4_8.c
+++ 120772_2022_4_8.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!\n");
+    FILE *subor;
+    FILE *s;
+    char ch,c,vypis;
+    subor =fopen("znaky.txt","r");
+    s = fopen("novy.txt","w");
+    scanf("%c",&ch);
+    //printf("%c",ch);
+    c = fgetc(subor);
+    while (c != EOF)
+    {
+        if(ch =='s'){
+        fputc(c, s);
+        c = fgetc(subor);}
+        else{
+            fscanf(subor,"%c",&vypis);
+            printf("%c",vypis);
+        }
+    }
+    fclose(subor);
+    fclose(s);
     return 0;
 }
