f7b69cc334f94a63337bf8c452edc1370b2f9326
Kristián Červenka <kristian.cervenka@gmail.com>
2022-10-13 20:33:50 +0200

    u9_dokoncene

--- 120772_2022_4_9.c
+++ 120772_2022_4_9.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!\n");
+    FILE *subor;
+    char c;
+    int p_m = 0; // počet medzier
+    subor =fopen("text.txt","r");
+
+    while((c = fgetc(subor)) != EOF)
+    {
+        if (c == ' ')
+            p_m++;
+        else if(c == '#' || c == '$' || c == '&')
+            printf("Precital som riadiaci znak\n");
+        else if (c == 'x' || c == 'X')
+            printf("Precital som X\n");
+        else if (c == 'y' || c == 'Y')
+            printf("Precital som Y\n");    
+        else if (c == '*')
+        {
+            printf("Koniec\n");
+            printf("Počet prečítaných medzier je: %d",p_m);
+            return 0;
+        }
+        
+            
+            
+    }
     return 0;
 }
