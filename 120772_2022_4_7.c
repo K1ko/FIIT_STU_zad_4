8a65d03a47ab1f0507b361841c88e3bd1e61f6b3
Kristián Červenka <kristian.cervenka@gmail.com>
2022-10-13 11:39:47 +0200

    dokonceny sucet cisel suboru

--- 120772_2022_4_7.c
+++ 120772_2022_4_7.c
@@ -5,7 +5,8 @@ int main()
     FILE *subor;
     char s;
     double x;
-    int i;
+    double sucet;
+    
     //fclose(fopen("cisla.txt","r"));
     subor = fopen("cisla.txt","r");
     if(subor == NULL)
@@ -14,19 +15,32 @@ int main()
         return 0;
         //exit();
     }
+    while(fscanf(subor,"%lf",&x)!=EOF){
+        printf("%.2lf\n",x);
+        sucet += x;
+    }
+    printf("sucet je %.2lf",sucet);
+    fclose(subor);
+    return 0;
+}
+    /*
     do
     {
-        s = getc(subor);
-        printf("%c",s);
+        s = fscanf(subor ,"%lf",&x);
+        //printf("%.2lf\n",x);
         //i = getc(subor);
-        x ++;
-        
+        //x ++;
+
+        sucet +=x;
+        //i++;
+       printf("%.2lf\n",x); 
     } while (s!=EOF);
+    printf("sucet cisel je %.2lf",sucet);
     fclose(subor);
     //printf("%lf \n",x);
     //printf("%d",x);
     //getch();
     
 }
-    
+    */
 
